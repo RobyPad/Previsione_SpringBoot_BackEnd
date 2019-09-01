f1dd15fc31566cdc3f0c963be58cde0518902e6e
A300085 <A300085@NTB2021421.ternaren.prv>
2019-09-01 16:19:46 +0200

    aaa

--- Previsione_SpringBoot_BackEnd/src/main/java/com/roberto/test/EsecuzioneMetodi.java
+++ Previsione_SpringBoot_BackEnd/src/main/java/com/roberto/test/EsecuzioneMetodi.java
@@ -11,6 +11,7 @@ public class EsecuzioneMetodi
 		
 		/*
 		 * Italia serie A 2016/2017
+		 * 
 		 */
 		String url_italia = "https://www.diretta.it/serie-a-2016-2017/";
 		String href_italia = "//a[@href='/serie-a-2016-2017/risultati/']";
